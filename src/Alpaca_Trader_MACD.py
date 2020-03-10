9311ea92aa6cc3212092c4724b26c53137e65cb2
Nathan <nathan.johnson999@aggiemail.usu.edu>
2020-03-10 08:26:01 -0600

    initial commit

---
+++ src/Alpaca_Trader_MACD.py
@@ -0,0 +1,59 @@
+import alpaca_trade_api as tradeapi
+
+'''
+This is meant to be used with cron tab so that it can run at the beginning or end
+of the day, whichever you choose.
+'''
+
+# to set this up with your alpaca account create a file named 'API.txt'
+# the file will contain 3 things 1; 'key', 2; 'secret key', 3; 'which market you want to trade paper or actual'
+f = open('API.txt', 'r')
+if f.mode == 'r':
+    file = f.read().split('\n')
+f.close()
+api = tradeapi.REST(file[0], file[1], file[2])
+
+
+def macd(symbol):
+    bar_set = api.get_barset(symbol, 'day', limit=4)
+    stock = bar_set[symbol]
+    day1 = stock[0].c
+    day2 = stock[1].c
+    day3 = stock[2].c
+    five_day_average = (day1 + day2 + day3) / 3
+    current = stock[3].o
+    if current > five_day_average:
+        return 'buy'
+    else:
+        return 'sell'
+
+
+def buy(symbol, quantity=1):
+    f = open('History', 'a+')
+    api.submit_order(symbol, quantity, 'buy', 'market', 'gtc')
+    f.write('buying: ' + symbol + ' at ' + ' quantity: ' + str(quantity))
+    f.close()
+
+
+def sell(symbol, quantity=1, limit_price=0):
+    f = open('History', 'a+')
+    if limit_price == 0:
+        api.submit_order(symbol, quantity, 'sell', 'limit', 'day', api.get_position(symbol).current_price)
+    else:
+        api.submit_order(symbol, quantity, 'sell', 'limit', 'day', limit_price)
+    f.write('\nselling: ' + symbol + ' at ' + ' quantity: ' + str(quantity))
+
+
+def main(symbol):
+    trade_or_not = macd(symbol)
+    if trade_or_not == 'sell':
+        sell(symbol, api.get_position(symbol).qty)
+        print(trade_or_not)
+    if trade_or_not == 'buy':
+        print('buy')
+        buy(symbol, float(api.get_account().buying_power) // api.get_barset('AMD', 'day', limit=1)['AMD'][0].c)
+        print(trade_or_not)
+
+
+print(macd('AMD'))
+main('AMD')
---
+++ src/MACD.py
@@ -0,0 +1,83 @@
+import csv
+
+
+def loader(filename):
+    with open(filename) as csv_file:
+        csv_reader = csv.reader(csv_file)
+        line_count = 0
+        daily_close = []
+        for row in csv_reader:
+            if line_count != 0:
+                daily_close.append(float(row[4]))
+            line_count += 1
+        # print(f'Processed {line_count} lines.')
+        return daily_close
+
+
+def macd(five_day, current_day):
+    total = 0
+    for i in five_day:
+        total += float(i)
+    moving_average = total / 3
+    if current_day > moving_average:
+        if margingains(moving_average, current_day):
+            return 'sell'
+        else:
+            return 'buy'
+        return 'buy'
+    else:
+        return 'sell'
+
+
+def margingains(moving_average, current_day):
+    if current_day > moving_average * 1.2:
+        return True
+    else:
+        return False
+
+
+
+def calculate(filename):
+    daily_close_list = loader(filename)
+    # print(daily_close_list)
+    x = 4
+    status = 1
+    account = 100
+    print('start account: ' + str(account))
+    buy_counter = 0
+    sell_counter = 0
+    while x < len(daily_close_list):
+        five_day_list = [daily_close_list[x-1], daily_close_list[x-2], daily_close_list[x-3]]
+        do_what = macd(five_day_list, daily_close_list[x])
+        if do_what == 'buy' and status == 0 and account >= daily_close_list[x]:
+            account = account - daily_close_list[x]
+            print('buy at: ' + str(daily_close_list[x]))
+            buy_counter += 1
+            status = 1
+        elif do_what == 'sell' and status == 1:
+            account += daily_close_list[x]
+            print('sell stock at: ' + str(daily_close_list[x]))
+            status = 0
+            sell_counter += 1
+        # print(account)
+        x += 1
+
+    print('amount of buys: ' + str(buy_counter))
+    print('amount of sells: ' + str(sell_counter))
+    print('account at day ' + str(x) + ': ' + str(account))
+    print('amount gain: ' + str(account - (100 + daily_close_list[0])))
+    print('percent gain: ' + str((account-(100 + daily_close_list[0])) / daily_close_list[0] *100) + '\n\n\n')
+    # print('done')
+
+
+print('testing MSFT:')
+calculate('MSFT.csv')
+
+print('testing TSLA:')
+calculate('TSLA.csv')
+
+print('testing AMD:')
+calculate('AMD.csv')
+
+print('testing IIPR:')
+calculate('IIPR.csv')
