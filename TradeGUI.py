ffb559f0a757648c84a4bd1c6ff770867fb057d4
Nathan Johnson <nathan9j@gmail.com>
2020-08-11 16:29:00 -0600

    editing gui graphics

--- TradeGUI.py
+++ TradeGUI.py
@@ -1,4 +1,4 @@
-import tkinter as tk
+from tkinter import Tk, Label, Button
 from tkinter import ttk
 from accountInfo import getAccount
 from menuFunctions import *
@@ -8,32 +8,41 @@ api = getAccount()
 '''
 GUI Creation
 '''
-app = tk.Tk()
+theme = ["#00897b", "#00564d", "#282828", "#363636", "#969696"]
+#   light_teal,      teal,      dark_grey,   grey,   light_grey
+app = Tk()
 app.title('TradeGUI')
-tab_menu = tk.ttk.Notebook(app)
-home = tk.ttk.Frame(tab_menu)
-trader = tk.ttk.Frame(tab_menu)
+tab_menu = ttk.Notebook(app)
+home = ttk.Frame(tab_menu)
+trader = ttk.Frame(tab_menu)
+testData = ttk.Frame(tab_menu)
 tab_menu.add(home, text='Home')
 tab_menu.add(trader, text='Trader')
-
+tab_menu.add(testData, text='Test Data')
 
 '''
 Home Tab menu
 '''
-accountName = tk.Label(home, text='Account: ' + api.get_account().account_number)
+accountName = Label(home, text='Account: ' + api.get_account().account_number)
 accountName.grid(row=0)
-amount_available = tk.Label(home, text='Available Money: ' + api.get_account().cash)
+amount_available = Label(home, text='Available Money: ' + api.get_account().cash)
 amount_available.grid(row=1, column=0)
-equity = tk.Label(home, text='Equity: ' + api.get_account().equity)
+equity = Label(home, text='Equity: ' + api.get_account().equity)
 equity.grid(row=2, column=0)
 
 '''
 Trader Tab menu
 '''
-MACDtext = tk.Label(home, text='Run MACD strategy: ')
+MACDtext = Label(home, text='Run MACD strategy: ')
 MACDtext.grid(row=0)
-MACDbutton = tk.Button(trader, text='MACD', command=MACDalgorithm)
+MACDbutton = Button(trader, text='MACD', command=MACDalgorithm)
 MACDbutton.grid(row=0, column=1)
+
+
+'''
+Test History Tab menu
+'''
+
 
 
 '''
