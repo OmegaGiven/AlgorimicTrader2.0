abd6b39eea47ebc823f0d9d38cfa9a0191995517
Nathan Johnson <nathan9j@gmail.com>
2020-08-11 16:00:04 -0600

    fixed up some file pathing

--- YahooHistoryDownloader.py
+++ YahooHistoryDownloader.py
@@ -1,12 +1,14 @@
-'''
+"""
 provided by https://medium.com/@jouneidraza522/yahoo-finance-api-to-get-stocks-tickers-data-in-python-c49820249a18
-'''
+edited by OmegaGiven to ve more current.
+"""
+
 from pandas_datareader import data as pdr
 from datetime import date
 import datetime
 import yfinance as yf
+
 yf.pdr_override()
-import pandas as pd
 # Tickers list
 # We can add and delete any ticker from the list to get desired ticker live data
 ticker_list = ['AMD']
@@ -14,7 +16,9 @@ today = date.today()
 # We can get data by our choice by giving days bracket
 start_date = datetime.datetime(2019, 8, 11)
 end_date = datetime.datetime(2020, 8, 11)
-files=[]
+files = []
+
+
 def getData(ticker):
     print(ticker)
     data = pdr.get_data_yahoo(ticker, start=start_date, end=today)
@@ -25,7 +29,7 @@ def getData(ticker):
 
 # Create a data folder in your current dir.
 def SaveData(df, filename):
-    df.to_csv(filename+'.csv')
+    df.to_csv('testFiles/' + filename+'.csv')
 
 
 # This loop will iterate over ticker list, will pass one ticker to get data, and save that data as file.
